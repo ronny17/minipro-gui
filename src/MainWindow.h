6984f72e1fe9d062df2cc243fd1569a486a0382a
Luca <43651170+ronny17@users.noreply.github.com>
2026-02-01 17:05:47 +0100

    Add new methods and buttons to MainWindow
    Signed-off-by: Luca <43651170+ronny17@users.noreply.github.com>

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -11,6 +11,7 @@
 #include <QRegularExpression>
 #include <QFileDialog>
 #include <QFile>
+#include <QDir>
 #include <QTableView>
 #include <QListWidget>
 #include <QLineEdit>
@@ -18,6 +19,9 @@
 #include <QComboBox>
 #include <QCheckBox>
 #include <QHeaderView>
+#include <QMessageBox>
+#include <QApplication>
+#include <QSettings>
 #include <iostream>
 #include <array>
 #include "HexView.h"
@@ -28,18 +32,20 @@ class MainWindow : public QMainWindow {
  public:
   explicit MainWindow(QWidget *parent = nullptr);
   ~MainWindow() override;
-
-  void check_for_minipro();
   bool minipro_found;
+
 
  private slots:
   void check_for_programmer();
+  void check_for_minipro();
   void get_devices();
   void select_device(const QString & = "");
   void async_process_err_output() const;
   void async_process_std_output() const;
   void read_device_output(int, QProcess::ExitStatus);
 
+  void select_output_path_dialog();
+  void select_path_dialog();
   void run_command();
   void read_device();
   void write_device();
@@ -53,13 +59,18 @@ class MainWindow : public QMainWindow {
   QFont monospace_font;
 
   const QString temp_file_name = "temp.bin";
+  QString save_file_name;
 
   QProcess *async_process;
 
   QPushButton *button_run_command;
+  QPushButton *btn_check_programmer;
   QPushButton *button_write;
   QPushButton *button_read;
   QPushButton *button_update;
+  QPushButton *button_test_minipro_path;
+  QPushButton *button_select_path;
+  QPushButton *button_select_output_path;
 
   QLabel *device_name_label;
   QLineEdit *device_name;
@@ -73,6 +84,10 @@ class MainWindow : public QMainWindow {
   QLineEdit *device_readbuffer;
   QLabel *device_writebuffer_label;
   QLineEdit *device_writebuffer;
+  QLabel *system_sett_path_label;
+  QLineEdit *system_sett_path;
+  QLabel *output_path_label;
+  QLineEdit *output_path;
   [[maybe_unused]] QLineEdit *device_other;
 
   bool programmer_found;
@@ -107,8 +122,10 @@ class MainWindow : public QMainWindow {
   void format_hex_table_columns() const;
   void disable_buttons();
   void enable_buttons();
+  void set_preferences(const QString &, QString &);
+  QString get_preferences(const QString &);
   [[nodiscard]] QStringList parse_checkboxes() const;
-  static QString run_process(QPlainTextEdit &, const QStringList &, const QString &);
+  QString run_process(QPlainTextEdit &, const QStringList &, const QString &);
   void run_async_process(QStringList &, const QString &);
   void build_default_hex_output();
   void build_formatted_hex_output();
